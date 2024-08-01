c52072e5ea36f33524ad2ecb7ce28e1b87d385e4
Sumanth B K <sumanth.kalikeri@gmail.com>
2024-08-01 23:28:38 +0530

    Update TCBScans.kt

--- src/en/tcbscans/src/eu/kanade/tachiyomi/revived/en/tcbscans/TCBScans.kt
+++ src/en/tcbscans/src/eu/kanade/tachiyomi/revived/en/tcbscans/TCBScans.kt
@@ -186,7 +186,7 @@ class TCBScans : ParsedHttpSource() {
                 }
             }
             .map { response ->
-                 throw Exception(response.body().string())
+                 throw Exception(response.body.string())
                 // pageListParse(response)
             }
     }
